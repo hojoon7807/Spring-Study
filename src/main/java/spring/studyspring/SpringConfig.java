520348f05d18aa8cbe0a4b74b7dcaea5dee8cbbc
hojoon <ujuj0202@gmail.com>
2022-01-25 22:47:19 +0900

    [ADD]: JPA repository 생성

--- src/main/java/spring/studyspring/SpringConfig.java
+++ src/main/java/spring/studyspring/SpringConfig.java
@@ -3,12 +3,11 @@ package spring.studyspring;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import spring.studyspring.repository.JdbcMemberRepository;
-import spring.studyspring.repository.JdbcTemplateMemberRespository;
-import spring.studyspring.repository.MemberRepository;
-import spring.studyspring.repository.MemoryMemberRepository;
+import spring.studyspring.repository.*;
 import spring.studyspring.service.MemberService;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.sql.DataSource;
 
 //직접 스프링 빈 등록하가
@@ -16,11 +15,12 @@ import javax.sql.DataSource;
 //정형화되지 않거나 상황에 따라 구현클래스를 변경해야될 경우 직접 빈 등록한다.
 @Configuration
 public class SpringConfig {
-    private DataSource dataSource;
+    //@PersistenceContext
+    private EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -32,6 +32,7 @@ public class SpringConfig {
     public MemberRepository memberRepository(){
         //return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRespository(dataSource);
+        //return new JdbcTemplateMemberRespository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
